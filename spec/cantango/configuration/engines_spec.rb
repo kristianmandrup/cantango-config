5e178b94eb7bf078ac5bb534749f851e0b186dd7
Kristian Mandrup <kmandrup@gmail.com>
2011-11-22 20:13:23 +0100

    starting engines spec

--- spec/cantango/configuration/engines_spec.rb
+++ spec/cantango/configuration/engines_spec.rb
@@ -1,8 +1,47 @@
 require 'rspec'
 require 'cantango'
+require 'cantango/configuration/shared/hash_registry_ex'
+
+class MyEngine < CanTango::Engine
+end
 
 describe CanTango::Configuration::Modes do
   subject { CanTango.config.engines }
+
+  it_should_behave_like "Hash Registry" do
+  end
   
-  pending 'TODO'
+  specify do 
+    lambda { subject.register :my_engine => 3 }.should raise_error
+  end
+  
+  specify do
+    lambda { subject.register :my_engine => MyEngine }.should_not raise_error
+  end
+  
+  describe '#registered' do
+    before do
+      subject.register :my_engine => MyEngine
+    end
+    
+    specify { subject.registered[:my_engine].should == MyEngine }
+  end
+
+  describe 'execution order' do
+    # execution_order= *names
+    # execute_first name
+    # execute_last name
+    # execute_before existing, name
+    # execute_after existing, name
+    # execution_order
+  end
+  
+  # available
+  # available? name
+  # all state
+  # any? state
+  # clear!
+  # each
+  # active? name
+  # active
 end

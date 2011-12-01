4f8e5d6917a1136f6bb2714b6c0a0f6775457f70
Kristian Mandrup <kristian@unity3d.com>
2011-12-01 09:58:01 +0100

    updated README

--- lib/cantango/configuration.rb
+++ lib/cantango/configuration.rb
@@ -26,7 +26,7 @@ module CanTango
 
     def ability
       @ability ||= conf::Ability.instance
-      @ability.default_class ||= CanTango::Ability::Executor::Base
+      @ability.default_class ||= CanTango::Ability::Executor::Base # from cantango-core
       @ability
     end
 

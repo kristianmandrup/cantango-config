f30e5dec96d3ce6fb014857e01e279b686f290b7
Kristian Mandrup <kmandrup@gmail.com>
2011-12-06 22:19:11 +0100

    better user and account registration

--- lib/cantango/configuration/accounts.rb
+++ lib/cantango/configuration/accounts.rb
@@ -2,6 +2,17 @@ module CanTango
   class Configuration
     class Accounts < Registry::Clazz
       include Singleton
+
+      def register_account clazz
+        register account_name(clazz), clazz
+      end
+
+      protected
+      
+      def account_name clazz
+        clazz.name.demodulize.gsub(/(.+)Account$/, '\1')..underscore
+      end
+      
     end
   end
 end
--- lib/cantango/configuration/users.rb
+++ lib/cantango/configuration/users.rb
@@ -2,6 +2,16 @@ module CanTango
   class Configuration
     class Users < Registry::Clazz
       include Singleton
+      
+      def register_user clazz
+        register user_name(clazz), clazz
+      end
+        
+      protected
+      
+      def user_name clazz
+        clazz.name.demodulize.gsub(/(.+)User$/, '\1')..underscore
+      end      
     end
   end
 end
